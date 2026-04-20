ce3e38335d14afae8c8a7659381c84d573ece101
RaghadNasiaf <naseefraghad@gmail.com>
2026-04-20 09:51:54 +0000

    Update task 2 with string literal types

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -58,5 +58,13 @@ function executeWork(employee: Director | Teacher): string {
   return employee.workTeacherTasks();
 }
 
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  return `Teaching ${todayClass}`;
+}
+
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
