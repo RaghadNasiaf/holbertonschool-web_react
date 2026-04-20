ecd44a15f9ffad3562566f1a9d78f4c1b6ad0d54
RaghadNasiaf <naseefraghad@gmail.com>
2026-04-20 09:10:45 +0000

    Update task 1 with StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -33,3 +33,37 @@ const printTeacher: printTeacherFunction = function (
 };
 
 console.log(printTeacher('John', 'Doe'));
+
+interface StudentClassInterface {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+  private firstName: string;
+
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const studentClass: StudentClassInterface = StudentClass;
+const student = new studentClass('John', 'Doe');
+
+console.log(student.displayName());
+console.log(student.workOnHomework());
